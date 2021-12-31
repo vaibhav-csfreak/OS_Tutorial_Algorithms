412e19d247863c88a1378984850551171399be16
Aagam Jain <57454613+aagamjain04@users.noreply.github.com>
2021-12-31 22:28:19 +0530

    update fifo_page_replacement.cpp

--- Fifo_Page_Replacement.cpp
+++ Fifo_Page_Replacement.cpp
@@ -1,42 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
+int frameCount,flag=-1;
+void display(queue<int>temp)
+{
+    int n=temp.size();
+   while(!temp.empty()){
+		cout<<temp.front()<<"\t";
+		temp.pop();
+	}
+    for(int i=0;i<frameCount-n;i++)
+    {
+        cout<<"-1"<<"\t";
+    }
+    if(flag==1)
+    {
+        cout<<"HIT";
+    }
+    else
+    {
+        cout<<"MISS";
+    }
+    cout<<endl;
 
-int pageFault(int pages[],int n,int framesCount)
+}
+
+int pageFault(vector<int>pages,int n,int framesCount)
 {
     queue<int> q;
     set<int> frame;
     int pageFault_count = 0;
-
+    
     for(int i=0;i<n;i++)
     {
         int pageNo = pages[i];
         auto it = frame.find(pageNo);
-        if(it==frame.end())
+        if(it==frame.end())   // checking if that pageNo is already in the frame or not
         {
-            if(frame.size()>=framesCount)
+            if(frame.size()>=framesCount)  // if frame is full then we remove the first page from the queue
             {
                  int replace = q.front();
-                 q.pop();
-                 frame.erase(replace);
+                 q.pop();                   // replaced page is removed from the queue
+                 frame.erase(replace);      // replaced page is removed from the set
             }
            
-            frame.insert(pageNo);
-            q.push(pageNo);
-            pageFault_count++;
+            frame.insert(pageNo); // new page is inserted in the set
+            q.push(pageNo);       // new page is inserted in the queue
+            pageFault_count++;   //increment page fault count
+            flag=0;
         }
+        else{flag=1;}
+
+        display(q);
     }
     return pageFault_count;
 
 }
 
+
 int main()
-{
-    int pages[] = {3,2,1,3,4,1,6,2,4,3,4,2,1,4,5,2,1,3,4};
-    int n = sizeof(pages)/sizeof(pages[0]);
+{  
+    cout<<"Enter the length of the refernce string and number of frames: ";
+
+    int n;
+
+    cin>>n>>frameCount;
     
-    int framesCount = 4;
-    
-    cout << pageFault(pages,n,framesCount) << "\n";
+    vector<int>pages(n);
+
+   // taking refernce string input
+
+   cout<<"Enter the reference string: ";
+
+    for(int i=0;i<n;i++)
+    {
+         cin>>pages[i]; 
+    }
+   cout<<endl<<"Frame Table"<<endl;
+   int t=pageFault(pages,n,frameCount);
+    cout <<"Total number of page faults are: " <<t<< "\n";
     return 0;
     
 }
